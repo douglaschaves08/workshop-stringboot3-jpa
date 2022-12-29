4723c5057173b23ea804b091ffdc2238d373513b
douglaschaves08 <dodochaves123@gmail.com>
2022-12-29 17:29:21 -0300

    Projeto criado

---
+++ src/main/java/dodochaves/course/CourseApplication.java
@@ -0,0 +1,13 @@
+ package dodochaves.course;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dodochaves/course/CourseApplicationTests.java
@@ -0,0 +1,13 @@
+package dodochaves.course;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
