95396039219b79365f8138968735c6610b9cba3a
douglaschaves08 <dodochaves123@gmail.com>
2023-01-02 16:19:39 -0300

    Camada de serviço e registro de componentes

---
+++ src/main/java/dodochaves/course/services/UserService.java
@@ -0,0 +1,28 @@
+package dodochaves.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import dodochaves.course.entities.User;
+import dodochaves.course.repositories.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll() {
+		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+	
+
+}
