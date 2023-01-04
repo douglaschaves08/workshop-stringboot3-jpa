6a0a9c275ab98cfbb805e286191130072273f333
douglaschaves08 <dodochaves123@gmail.com>
2023-01-03 23:07:36 -0300

    Tratamento de exceção, findById

--- src/main/java/dodochaves/course/services/UserService.java
+++ src/main/java/dodochaves/course/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import dodochaves.course.entities.User;
 import dodochaves.course.repositories.UserRepository;
+import dodochaves.course.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -21,7 +22,7 @@ public class UserService {
 	
 	public User findById(Long id) {
 		Optional<User> obj = repository.findById(id);
-		return obj.get();
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 	
 	public User insert(User obj) {
