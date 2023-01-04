05a5324018356a208a4cba3078f8a09a346cc16c
douglaschaves08 <dodochaves123@gmail.com>
2023-01-03 22:04:48 -0300

    Pagamento, asssociação um para um

--- src/main/java/dodochaves/course/config/TestConfig.java
+++ src/main/java/dodochaves/course/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import dodochaves.course.entities.Category;
 import dodochaves.course.entities.Order;
 import dodochaves.course.entities.OrderItem;
+import dodochaves.course.entities.Payment;
 import dodochaves.course.entities.Product;
 import dodochaves.course.entities.User;
 import dodochaves.course.entities.enums.OrderStatus;
@@ -80,5 +81,10 @@ public class TestConfig implements CommandLineRunner {
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 	}
 }
